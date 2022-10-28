637f4fb572c562840b9178b0de788e3e1e58da85
liuxess <lukasjankauskasthelj@gmail.com>
2022-10-28 16:27:29 +0300

    Added library

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import PageRouting from './pages/routing/PageRouting';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <React.Fragment>
-        <PageRouting />{' '}
+        <PageRouting />
+        <ToastContainer />
     </React.Fragment>
   );
 }
